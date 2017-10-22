623a7feb096a567d5c0b956ba4a007bdd5077833
TheGuyThatMadeAnAccountToTestMarkdown <32992850+TheGuyThatMadeAnAccountToTestMarkdown@users.noreply.github.com>
2017-10-21 22:42:40 -0400

    example.java

---
+++ example.java
@@ -0,0 +1,11 @@
+public class Example {
+    public static int bin() {
+        //code
+    }
+    public static int[] binFunctionTwo() {
+        //code
+    }
+    public static void main(String[] args) {
+        //code
+    }
+}
